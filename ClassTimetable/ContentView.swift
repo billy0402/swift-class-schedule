b28009ebd0240c5d2195b1fa129ece9f9fcd38c9
Billy Huang <10646003@ntub.edu.tw>
2020-07-21 12:02:07 +0800

    feat(main): add swiftui

---
+++ ClassTimetable/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  ClassTimetable
+//
+//  Created by Billy on 2020/7/20.
+//  Copyright © 2020 NTUB. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct ContentView: View {
+    @State private var studentId = ""
+
+    var body: some View {
+        VStack {
+        TextField("學號", text: $studentId)
+            HStack {
+                Text("節次")
+                    .frame(width: 100)
+                    .aspectRatio(3/2, contentMode: .fill)
+                ScrollView {
+                    Text("ScrollView")
+                }
+            }
+            HStack {
+                ScrollView {
+                    Text("ScrollView")
+                }
+                ScrollView {
+                    Text("ScrollView")
+                }
+            }
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
